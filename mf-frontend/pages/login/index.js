0fae82f34a5016bfae18d045eff4390589182e0a
steve chak <stevechakcy@gmail.com>
2021-10-21 11:26:40 +0800

    add login page back route if is logined

--- mf-frontend/pages/login/index.js
+++ mf-frontend/pages/login/index.js
@@ -1,10 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import React from "react";
+import React, {useContext, useEffect} from "react";
 import {LoginPanel} from "../../components/LoginPanel";
+import {useRouter} from "next/router";
+import {AuthContext} from "../../context/authContext";
 
 
 export default function Login(){
+    const router = useRouter()
+    const {user , login} = useContext(AuthContext)
+    useEffect(()=>{
+        if(user["authReady"]){
+            router.back()
+        }
+    },[])
     return(
         <div className={"login-layout"}>
             <LoginPanel />
