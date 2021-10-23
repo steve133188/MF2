a0a055f6640bfa89b3a729fb06ac77653a50925d
BEN <ben@BENs-MacBook-Pro.local>
2021-10-24 01:20:38 +0800

    backend commit

--- Backend/Bot/Services/botService.go
+++ Backend/Bot/Services/botService.go
@@ -6,30 +6,35 @@ import (
 	"fmt"
 	"mf-bot-services/Model"
 	"net/http"
-	"reflect"
 	"sync"
 )
 
 var Mu = sync.Mutex{}
-var Botlist = make(map[int]string)
-var Stages = make(map[string]interface{})
+var Botlist = make(map[int]string)        // number : senderid
+var Stages = make(map[string]interface{}) // senderid : []Stages
+var Steps = make(map[string]string)       // senderid : location id in MongoDB
 var Num = 0
 
 func HandleWhatsappResponse(resp http.ResponseWriter, req *http.Request) {
 	//send request body to Flow
+	senderId := req.URL.Query().Get("sender_id")
+	message := req.URL.Query().Get("message")
+
+	fmt.Println(senderId, message)
+
 	clt := http.Client{}
 	data := new(Model.BotBody)
 	dec := json.NewDecoder(req.Body)
 	err := dec.Decode(&data)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("1 :", err)
 	}
 
 	parseData, err := json.Marshal(data)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("2 ", err)
 	}
-	// fmt.Println(bytes.NewBuffer(parseData))
+
 	newReq, err := http.NewRequest("POST", "http://localhost:3006", bytes.NewBuffer(parseData))
 	if err != nil {
 		fmt.Println(err)
@@ -40,6 +45,12 @@ func HandleWhatsappResponse(resp http.ResponseWriter, req *http.Request) {
 		fmt.Println(err)
 	}
 
+	//test
+	// parData, err := json.Marshal(data.Stages)
+	// if err != nil {
+	// 	fmt.Println("3 ", err)
+	// }
+
 	// mapping
 	// Mu.Lock()
 	hasBot := false
@@ -48,44 +59,108 @@ func HandleWhatsappResponse(resp http.ResponseWriter, req *http.Request) {
 			hasBot = true
 		}
 	}
-	if hasBot == false {
-		Botlist[Num] = data.SenderId
-		Stages[data.SenderId] = data.Stages
+	if !hasBot {
+		Botlist[Num] = senderId
+		Stages[senderId] = data.Stages
+		// Stages[data.SenderId] = parData
 		Num++
 	}
+
 	// Mu.Unlock()
-	sendResponse(data.SenderId)
+	sendResponse(senderId, message)
 
 }
 
-func sendResponse(target string) {
+func sendResponse(target string, message string) {
+	var todo = Stages[target]
 
-	data := new(Model.Stages)
-	stages := Stages[target]
-	// var result []struct{}
+	var stages []Model.Stages
+	var stage Model.Stages
 
-	// var todos []Model.Stages = make([]Model.Stages, 0)
-	//interface -> []struct{}
-	switch reflect.TypeOf(stages).Kind() {
-	case reflect.Slice:
-		s := reflect.ValueOf(stages)
-		for i := 0; i < s.Len(); i++ {
-			fmt.Println(s.Index(i))
-			step, err := json.Marshal(s.Index(i))
+	// var actions []Model.Actions
+	var action Model.Actions
+
+	// get []stage data
+	if sl, ok := todo.([]interface{}); ok {
+		for _, v := range sl {
+			mapData := v.(map[string]interface{})
+			b, err := json.Marshal(mapData)
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println("send response marshal error")
 			}
-			fmt.Println(string(step))
-
-			err = json.Unmarshal(step, &data)
+			err = json.Unmarshal(b, &stage)
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println("send response unmarshal error")
 			}
-			// result = append(result, *bytes.NewBuffer(step))
+			stages = append(stages, stage)
+			// fmt.Println("/////////")
+			// test, _ := json.Marshal(stage)
+			// fmt.Println(k, bytes.NewBuffer(test))
+		}
+		// test, _ := json.Marshal(stages)
+		// fmt.Println(bytes.NewBuffer(test))
+	}
+
+	//get get source id from stage
+	Steps[target] = "12314"
+	if Steps[target] == "" {
+		if sl, ok := stages[0].Actions.([]interface{}); ok {
+			for k, v := range sl {
+				mapData := v.(map[string]interface{})
+				b, err := json.Marshal(mapData)
+				if err != nil {
+					fmt.Println("send response marshal error")
+				}
+				err = json.Unmarshal(b, &action)
+				if err != nil {
+					fmt.Println("send response unmarshal error")
+				}
+				fmt.Println("/////////")
+				test, _ := json.Marshal(action)
+				fmt.Println(k, bytes.NewBuffer(test))
+			}
+			// test, _ := json.Marshal(stages)
+			// fmt.Println(bytes.NewBuffer(test))
+		}
+		Steps[target] = action.Id
+	} else {
+		for i := 0; i < len(stages); i++ {
+			if sl, ok := stages[i].Actions.([]interface{}); ok {
+				for _, v := range sl {
+					mapData := v.(map[string]interface{})
+					b, err := json.Marshal(mapData)
+					if err != nil {
+						fmt.Println("send response marshal error")
+					}
+					err = json.Unmarshal(b, &action)
+					if err != nil {
+						fmt.Println("send response unmarshal error")
+					}
+					fmt.Println(i, " ", action.Id)
+				}
+				// test, _ := json.Marshal(stages)
+				// fmt.Println(bytes.NewBuffer(test))
+			}
 		}
 	}
 
-	// todos = result
+	//testing
+	// s := reflect.ValueOf(todo)
+	// b := s.Interface().([]byte)
+
+	// err := json.Unmarshal(b, &todos)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// for _, v := range todos {
+	// 	// fmt.Println(k, v.Actions)
+	// 	// fmt.Println("//////////")
+	// 	// fmt.Println(reflect.TypeOf(v.Actions))
+	// 	val := reflect.ValueOf(v.Actions)
+	// 	fmt.Println(val.Interface().([]byte))
+
+	// }
+
 	//send response
 	//http.NewRequest("POST", "https://bot.stellabot.com/sendResponses")
 
--- Backend/Bot/go.mod
+++ Backend/Bot/go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/gofiber/fiber/v2 v2.19.0
+	github.com/goinggo/mapstructure v0.0.0-20140717182941-194205d9b4a9
 	github.com/joho/godotenv v1.3.0
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d
 	go.mongodb.org/mongo-driver v1.7.2
