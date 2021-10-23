7201bcc4462091f550a2407d80c28fb8c1d77897
steve chak <stevechakcy@gmail.com>
2021-10-24 01:41:15 +0800

    change font , color ; create sidebar component

--- mf-frontend/layouts/Layout.js
+++ mf-frontend/layouts/Layout.js
@@ -1,7 +1,8 @@
-
+import {navItems} from "./nav-item";
 import {useRouter} from "next/router";
 import {useEffect , useContext} from "react";
 import {AuthContext} from "../context/authContext"
+import SideBar from "./SideBar";
 export  default function Layout ({children}){
     const router = useRouter()
     const {user , login} = useContext(AuthContext)
@@ -11,13 +12,13 @@ export  default function Layout ({children}){
 
         </div>
     )
-    useEffect(()=>{
-        if(!user["authReady"]){
-            router.push("/login")
-            layout = (<div className={"unauth"}>{children}</div>)
-        }
-    },[])
+    // useEffect(()=>{
+    //     if(!user["authReady"]){
+    //         router.push("/login")
+    //         layout = (<div className={"unauth"}>{children}</div>)
+    //     }
+    // },[])
     return(
-        !user["authReady"] ?<div className={"layout"}>{children}</div>:<div className={"unauth"}>{children}</div>
+        !user["authReady"] ?<div className={"layout"}><SideBar navItems={navItems} />{children}</div>:<div className={"unauth"}>{children}</div>
     )
 }
--- mf-frontend/pages/dashboard/index.js
+++ mf-frontend/pages/dashboard/index.js
@@ -1,10 +1,18 @@
+import {LineChart} from "../../components/LineChart";
+
 export default function Dashboard() {
     return(
-        <div className="dashboardContainer">
-            <img src="./001.png" alt=""/>
-            <img src="./002.png" alt=""/>
-            <img src="./003.png" alt=""/>
-            <img src="./dashBackground.png" alt=""/>
-        </div>
+        <>
+            <div id={"dashboard"}>
+                <LineChart />
+            </div>
+            <div className="dashboardContainer">
+                <img src="./001.png" alt=""/>
+                <img src="./002.png" alt=""/>
+                <img src="./003.png" alt=""/>
+                <img src="./dashBackground.png" alt=""/>
+            </div>
+        </>
+
     )
 }
