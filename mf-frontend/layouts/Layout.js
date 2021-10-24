e4c68b84a9f804554d61b7fa41121e3e21154b21
lewis221 <lewischan221@gmail.com>
2021-10-24 18:01:47 +0800

    sysc

--- mf-frontend/layouts/Layout.js
+++ mf-frontend/layouts/Layout.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-
-=======
 import {navItems} from "./nav-item";
->>>>>>> caec76d974697c3fd6f4dfbb9fc97ae936d2db12
 import {useRouter} from "next/router";
 import {useEffect, useContext, useState} from "react";
 import {AuthContext} from "../context/authContext"
@@ -11,14 +7,7 @@ export  default function Layout ({children}){
     const router = useRouter()
     const {user , login} = useContext(AuthContext)
     let layout =(
-<<<<<<< HEAD
-        <div className={"layout"}>
-            {children}
-
-        </div>
-=======
         <div className={"layout"}><SideBar navItems={navItems} /><div className={"layout-main"}><LayoutTop page_title={"Dashboard"} />{children}</div></div>
->>>>>>> caec76d974697c3fd6f4dfbb9fc97ae936d2db12
     )
     let unAuth = (<div className={"unauth"}>{children}</div>)
     // useEffect(()=>{
@@ -48,8 +37,11 @@ function LayoutTop(props){
                         <path
                             d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
                     </svg>
-                    {isNotificationShow ? (<div className="dropdown-menu notification-menu">
+                    {isNotificationShow ? (
+                    <div className="dropdown-menu notification-menu">
                        <button className={" dropdown-item "}>notifications</button>
+                       <button className={" dropdown-item "}>notifications2</button>
+                       <button className={" dropdown-item "}>notifications3</button>
                     </div>):null }
                 </button>
 
