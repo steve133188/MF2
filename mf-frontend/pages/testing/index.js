abb1089d61f7129d0ab452405aa16d6080933d58
steve chak <stevechakcy@gmail.com>
2021-10-25 01:28:01 +0800

    cannot fix the apexchart isuess

--- mf-frontend/pages/testing/index.js
+++ mf-frontend/pages/testing/index.js
@@ -1,32 +1,42 @@
 import {BlueMenu, BlueMenuGroup} from "../../components/BlueMenu";
 import {BlueMenuDropdown, BlueMenuLink} from "../../components/BlueMenuLink";
-import dynamic from "next/dynamic";
-import {ChangingPercentageCard} from "../../components/Cards";
-
-const LineChart1 = dynamic(
-    () => import('../../components/LineChart1').then(mod => mod.LineChart1),
-    {ssr: false}
-);
-
-const MultipleLineChart = dynamic(
-    () => import('../../components/LineChart1').then(mod => mod.MultipleLineChart),
-    {ssr: false}
-);
-
-const LineChartCard = dynamic(
-    () => import('../../components/Cards').then(mod => mod.LineChartCard),
-    {ssr: false}
-);
-
-
+// import dynamic from "next/dynamic";
+// import {ChangingPercentageCard} from "../../components/Cards";
+// import {useEffect} from "react";
+//
+// const LineChart1 = dynamic(
+//     async () => {
+//         return await  import('../../components/LineChart1').then(mod => mod.LineChart1)
+//     },
+//     {ssr: false}
+// );
+//
+// const MultipleLineChart = dynamic(
+//     async () => {
+//         return await  import('../../components/LineChart1').then(mod => mod.MultipleLineChart)
+//     },
+//     {ssr: false}
+// );
+//
+// const LineChartCard = dynamic(
+//     async () => {
+//         return await import('../../components/Cards').then(mod => mod.LineChartCard)
+//     },
+//     {ssr: false}
+// );
+//
+//
 export default function Testing() {
+    // useEffect(()=>{
+    //
+    // },[])
 
     return (
         <>
-            <LineChart1/>
-            <MultipleLineChart/>
-            <LineChartCard/>
-            <ChangingPercentageCard />
+            {/*<LineChart1/>*/}
+            {/*<MultipleLineChart/>*/}
+            {/*<LineChartCard/>*/}
+            {/*<ChangingPercentageCard />*/}
         </>
     )
 }
