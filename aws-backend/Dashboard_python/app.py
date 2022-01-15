2613f2fc57f458aada5ee461a8ab7a4faef38149
Ben <chb514276128@gmail.com>
2022-01-16 01:34:11 +0800

    update

--- aws-backend/Dashboard_python/app.py
+++ aws-backend/Dashboard_python/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+# from output import Output
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
+    # data = Output.construct_data()
+    # print(data)
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True)
