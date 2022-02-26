28aa6734d9bc026bda3d88c35a00f8185430e3c6
LUNTNT <93472532+LUNTNT@users.noreply.github.com>
2022-02-27 04:55:10 +0800

    dummy save

--- Backend_v2/ChatbotAPI/config/redis-config.go
+++ Backend_v2/ChatbotAPI/config/redis-config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"context"
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"github.com/go-redis/redis/v8"
 	"log"
@@ -17,18 +18,23 @@ var ChatBotDB *redis.Client
 func RedisInit() {
 	MemoryDBInit()
 	ChatBotDBInit()
+	SaveDummy()
 }
+
 var MEMORYDBURL = []string{"clustercfg.mf2-redis.2j4s5t.memorydb.ap-east-1.amazonaws.com:6379"}
 
-var CHATBOTDBURL ="127.0.0.1:6379"
+var CHATBOTDBURL = "127.0.0.1:6379"
+
 //"matrixsense:automations:1:0:#1"
 var dummyOption1 = []string{
 	"matrixsense:actions:00000000002",
 }
+
 //"matrixsense:automations:1:0:#2"
 var dummyOption2 = []string{
 	"matrixsense:actions:00000000003",
 }
+
 //"matrixsense:automations:1:0:#3"
 var dummyOption3 = []string{
 	"matrixsense:actions:00000000004",
@@ -36,42 +42,45 @@ var dummyOption3 = []string{
 
 //"matrixsense:actions:00000000004"
 var dummyReplyAction2 = map[string]interface{}{
-	"type": "REPLY",
+	"type":     "REPLY",
 	"consumer": "customer or message ",
 	"payload":  map[string]interface{}{"body": "Lun Height :183"},
 }
+
 //"matrixsense:actions:00000000003"
 var dummyReplyAction3 = map[string]interface{}{
-	"type": "REPLY",
+	"type":     "REPLY",
 	"consumer": "customer or message ",
 	"payload":  map[string]interface{}{"body": "Ben Height : 170"},
 }
+
 //"matrixsense:actions:00000000002"
 var dummyReplyAction4 = map[string]interface{}{
-	"type": "REPLY",
+	"type":     "REPLY",
 	"consumer": "customer or message ",
 	"payload":  map[string]interface{}{"body": "Steve Height :180"},
 }
+
 //"matrixsense:actions:00000000000"
 var errorHandleAction = map[string]interface{}{
-	"type": "REPLY",
+	"type":     "REPLY",
 	"consumer": "message ",
 	"payload":  map[string]interface{}{"body": "Sorry I dont understand please try agein"},
 }
 
 //"matrixsense:actions:0000000000001"
 var defaultAction = map[string]interface{}{
-	"type": "REPLY",
+	"type":     "REPLY",
 	"consumer": "message ",
 	"payload":  map[string]interface{}{"body": "Hi I'm MF Assistant \n What can I help you? \n  Please type and rely following number of items \n 1. How tall is Steve\n 1. How tall is Ben\n1. How tall is Lun\n"},
 }
 
 //"matrixsense:flows:0000000000000"
 var dummyFlow = map[string]interface{}{
-	"flowName": "flow1",
+	"flowName":  "flow1",
 	"companyId": "matrixsense",
-	"length": 1,
-	"flow":[][]string{
+	"length":    1,
+	"flow": [][]string{
 		[]string{
 			"matrixsense:automations:1:0:#1",
 			"matrixsense:automations:1:0:#2",
@@ -82,22 +91,57 @@ var dummyFlow = map[string]interface{}{
 	"update_at": "0000000000",
 	"create_by": "matrixsense",
 	"update_by": "tiffany",
-	"default": "matrixsense:actions:0000000000001",
-	"timeout": map[string]interface{}{"duration": "10s" , "action": []string{
+	"default":   "matrixsense:actions:0000000000001",
+	"timeout": map[string]interface{}{"duration": "10s", "action": []string{
 		"tiffany:actions:00000000000",
 	}},
 }
 
+func SaveDummy() {
 
-func MemoryDBInit(){
-	var ctx,cancel = context.WithTimeout(context.Background() ,time.Second*3)
+	pipe := MemoryDB.Pipeline()
+	//flow
+	flow, _ := json.Marshal(dummyFlow)
+	pipe.HSet(context.Background(), "matrixsense:flows:0000000000000", flow)
+
+	//option
+	option1, _ := json.Marshal(dummyOption1)
+	pipe.HSet(context.Background(), "matrixsense:automations:1:0:#1", option1)
+	option2, _ := json.Marshal(dummyOption2)
+	pipe.HSet(context.Background(), "matrixsense:automations:1:0:#2", option2)
+	option3, _ := json.Marshal(dummyOption3)
+	pipe.HSet(context.Background(), "matrixsense:automations:1:0:#3", option3)
+
+	//action
+	defaultaction, _ := json.Marshal(defaultAction)
+	pipe.HSet(context.Background(), "matrixsense:actions:0000000000001", defaultaction)
+
+	errcation, _ := json.Marshal(errorHandleAction)
+	pipe.HSet(context.Background(), "matrixsense:actions:00000000000", errcation)
+
+	action2, _ := json.Marshal(dummyReplyAction2)
+	pipe.HSet(context.Background(), "matrixsense:actions:00000000004", action2)
+	action3, _ := json.Marshal(dummyReplyAction3)
+	pipe.HSet(context.Background(), "matrixsense:actions:00000000003", action3)
+	action4, _ := json.Marshal(dummyReplyAction4)
+	pipe.HSet(context.Background(), "matrixsense:actions:00000000002", action4)
+
+	_, err := pipe.Exec(context.Background())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func MemoryDBInit() {
+	var ctx, cancel = context.WithTimeout(context.Background(), time.Second*3)
 
 	defer cancel()
 
 	ctx.Done()
-	addrs  := strings.Split(os.Getenv("MEMORYDBURL")," ")
+	addrs := strings.Split(os.Getenv("MEMORYDBURL"), " ")
 
-	if len(addrs) ==0 || addrs == nil{
+	if len(addrs) == 0 || addrs == nil {
 		addrs = MEMORYDBURL
 	}
 
@@ -135,21 +179,20 @@ func MemoryDBInit(){
 
 }
 
-func ChatBotDBInit(){
-	var ctx,cancel = context.WithTimeout(context.Background() ,time.Second*5)
+func ChatBotDBInit() {
+	var ctx, cancel = context.WithTimeout(context.Background(), time.Second*5)
 
 	defer cancel()
 
-	addr :=os.Getenv("CHATBOTDBURL")
+	addr := os.Getenv("CHATBOTDBURL")
 
-	if  addr == "" {
+	if addr == "" {
 		addr = CHATBOTDBURL
 	}
 
 	ChatBotDB = redis.NewClient(&redis.Options{
 		Addr: addr,
 	})
-
 
 	pong, err := ChatBotDB.Ping(ctx).Result()
 	log.Println(pong, err)
