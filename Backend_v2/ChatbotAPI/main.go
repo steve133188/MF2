59d76cc8b3043cc15d76a591b275f51e44d1d2de
steve133188 <steve133188@gmail.com>
2022-02-27 21:50:45 +0800

    pull

--- Backend_v2/ChatbotAPI/main.go
+++ Backend_v2/ChatbotAPI/main.go
@@ -4,11 +4,13 @@ import (
 	"ChatbotAPI/config"
 	"ChatbotAPI/hanlder"
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"net/http"
+	"reflect"
 )
 
 func main() {
@@ -26,7 +28,30 @@ func main() {
 		for{
 			for msg := range messagesSub.Channel() {
 
-				fmt.Printf("channel=%s message=%s\n", msg.Channel, msg.Payload)
+				data ,err :=json.Marshal(msg.Payload)
+
+				fmt.Println(msg.Pattern)
+				fmt.Println(msg.PayloadSlice)
+				fmt.Println(msg.String())
+				fmt.Println(msg.Payload)
+
+				dataMap :=map[string]interface{}{
+
+				}
+				if err != nil{
+					fmt.Printf("Marshall failed=%s\n",err)
+				}
+				err = json.Unmarshal(data ,&dataMap);if err != nil {
+					fmt.Printf("UNMarshall failed=%s\n",err)
+				}
+
+				for i ,v :=range dataMap{
+					fmt.Println( i, v)
+
+				}
+
+				fmt.Println( dataMap, reflect.TypeOf(dataMap))
+				//config.ChatBotDB.Set(context.Background() , key , )
 
 			}
 		}
@@ -43,6 +68,13 @@ func main() {
 	api.Post("/flow", hanlder.CreateFlow)
 	api.Post("/action", hanlder.CreateAction)
 	api.Post("/option", hanlder.CreateOption)
+
+	app.Post("/" , func(c *fiber.Ctx) error {
+
+		config.ChatBotDB.Publish(context.Background() ,"messages.received" , c.Body())
+
+		return c.Status(http.StatusOK).SendString("received data")
+	})
 
 	app.Listen(":3010")
 
